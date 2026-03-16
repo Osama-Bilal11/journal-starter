a22239bc33c04fd8aa90765fdb78b905789008c1
oalsheik <247484098+oalsheik@users.noreply.github.com>
2026-03-16 04:05:23 +0000

    added logging

--- api/main.py
+++ api/main.py
@@ -1,12 +1,13 @@
-
+from api.routers.journal_router import router as journal_router
+from fastapi import FastAPI
+import logging
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-from fastapi import FastAPI
-
-from api.routers.journal_router import router as journal_router
-
+console = logging.StreamHandler()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('myLogger')
 
 # TODO: Setup basic console logging
 # Hint: Use logging.basicConfig() with level=logging.INFO
@@ -16,5 +17,8 @@ from api.routers.journal_router import router as journal_router
 # 3. Add console handler
 # 4. Test by adding a log message when the app starts
 
-app = FastAPI(title="Journal API", description="A simple journal API for tracking daily work, struggles, and intentions")
+app = FastAPI(title="Journal API",
+              description="A simple journal API for tracking daily work, struggles, and intentions")
 app.include_router(journal_router)
+
+logger.info("Test log")
